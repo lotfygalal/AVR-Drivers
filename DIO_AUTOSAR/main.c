45707b9f13310375fc09b784660ec0d98c22ef54
lotfygalal <123119488+lotfygalal@users.noreply.github.com>
2024-03-07 22:47:04 +0200

    Add files via upload

---
+++ DIO_AUTOSAR/main.c
@@ -0,0 +1,15 @@
+								/****************** Author: Lotfy galal   **************************/
+#include "Common_Macros.h"
+#include "Dio.h"
+#include "Dio_Regs.h"
+
+
+
+int main(void)
+{
+	while(1)
+	{	
+		
+	}
+	return 0;
+}
